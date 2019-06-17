4a18fcf8c2e84fa790c0e8baa3d6319d144e75cf
April17 <fanmeng0417@gmail.com>
2019-06-17 09:21:06 -0400

    Done.

--- spec/controllers/session_controller_spec.rb
+++ spec/controllers/session_controller_spec.rb
@@ -3,25 +3,25 @@ require 'rails_helper'
 RSpec.describe SessionsController, type: :controller do
   render_views
   describe 'post create' do
-    it 'redirects to the login page if :name is nil' do
-      post :create
-      expect(response).to redirect_to controller: 'sessions', action: 'new'
-    end
-
-    it 'redirects to login page if :name is empty' do
-      post :create, params: { name: '' }
-      expect(response).to redirect_to controller: 'sessions', action: 'new'
-    end
+    # it 'redirects to the login page if :username is nil' do
+    #   post :create
+    #   expect(response).to redirect_to controller: 'sessions', action: 'new'
+    # end
+    #
+    # it 'redirects to login page if :username is empty' do
+    #   post :create, params: { username: '' }
+    #   expect(response).to redirect_to controller: 'sessions', action: 'new'
+    # end
 
     it 'sets session[:name] if :name was given' do
       me = 'Werner Brandes'
-      post :create, params: { name: me }
-      expect(@request.session[:name]).to eq me
+      post :create, params: { username: me }
+      expect(@request.session[:username]).to eq me
     end
 
     it 'redirects to "/" if logged in' do
       me = 'Werner Brandes'
-      post :create, params: { name: me }
+      post :create, params: { username: me }
       expect(response).to redirect_to('/')
     end
 
@@ -29,16 +29,16 @@ RSpec.describe SessionsController, type: :controller do
   end
 
   describe 'post destroy' do
-    it 'leaves session[:name] nil if it was not set' do
+    it 'leaves session[:username] nil if it was not set' do
       post :destroy
-      expect(@request.session[:name]).to be nil
+      expect(@request.session[:username]).to be nil
     end
 
-    it 'clears session[:name] if it was set' do
-      post :create, params: { name: 'Trinity' }
-      expect(@request.session[:name]).not_to be nil
+    it 'clears session[:username] if it was set' do
+      post :create, params: { username: 'Trinity' }
+      expect(@request.session[:username]).not_to be nil
       post :destroy
-      expect(@request.session[:name]).to be nil
+      expect(@request.session[:username]).to be nil
     end
   end
 end
